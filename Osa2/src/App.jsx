d8571a2e88450ebc9610e542e1ab9cd9a8794b89
Jani <jankuul@gmail.com>
2023-08-13 23:21:57 +0300

    invalid hook call bugi perkele backup

--- Osa2/src/App.jsx
+++ Osa2/src/App.jsx
@@ -1,97 +1,36 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function useTestEffect() {
+  useEffect(() => {
+    console.log("Effect has been run");
+  }, []);
+}
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  // Call the custom hook
+  useTestEffect();
 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [searchTerm, setSearchTerm] = useState('');
+  /*
+  const [persons, setPersons] = useState([]);
 
-  const handleSearchChange = (event) => setSearchTerm(event.target.value);
-  const handleNameChange = (event) => setNewName(event.target.value);
-  const handleNumberChange = (event) => setNewNumber(event.target.value);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const nameExists = persons.some(person => person.name === newName);
-
-    if (nameExists) {
-      alert(`${newName} is already added to phonebook`);
-    } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }));
-      setNewName('');
-      setNewNumber('');
-    }
-  }
-
-  const filteredPersons = persons.filter(person => 
-    person.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+  }, []);
+  */
   return (
     <div>
       <h2>Phonebook</h2>
-
-      <FilterForm searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
-      <NewPersonForm 
-        handleSubmit={handleSubmit} 
-        newName={newName} 
-        handleNameChange={handleNameChange} 
-        newNumber={newNumber} 
-        handleNumberChange={handleNumberChange} 
-      />
-
-      <h2>Numbers</h2>
-      <Persons persons={filteredPersons} />
-
+      {/* {persons.map(person => 
+        <div key={person.id}>
+          {person.name} {person.number}
+        </div>
+      )} */}
     </div>
   )
 }
-
-const FilterForm = ({ searchTerm, handleSearchChange }) => {
-  return (
-    <div>
-      filter shown with: <input value={searchTerm} onChange={handleSearchChange} />
-    </div>
-  );
-}
-
-const NewPersonForm = ({ handleSubmit, newName, handleNameChange, newNumber, handleNumberChange }) => {
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        name: <input value={newName} onChange={handleNameChange} />
-      </div>
-      <div>
-        number: <input value={newNumber} onChange={handleNumberChange} />
-      </div>
-      <div>
-        <button type="submit">add</button>
-      </div>
-    </form>
-  );
-}
-
-const Person = ({ person }) => {
-  return (
-    <p>{person.name} {person.number}</p>
-  );
-}
-
-const Persons = ({ persons }) => {
-  return (
-    <div>
-      {persons.map(person => <Person key={person.name} person={person} />)}
-    </div>
-  );
-}
-
-
 
 export default App;
