229fd06876c97226eb3461165bedbdab36a28f51
Jani <jankuul@gmail.com>
2023-08-13 18:13:18 +0300

    Osa2, kurssitiedot

---
+++ Osa2/_kurssitiedot/App.jsx
@@ -0,0 +1,89 @@
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      {courses.map(course => <Course key={course.id} course={course} />)}
+    </div>
+  )
+}
+
+const Header = ({ courseName }) => {
+  return (
+    <h1>{courseName}</h1>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Course = ({ course }) => {
+  const getTotalExercises = () => course.parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <p><b> total of {getTotalExercises()} exercises</b></p>
+    </div>
+  )
+}
+
+
+export default App
