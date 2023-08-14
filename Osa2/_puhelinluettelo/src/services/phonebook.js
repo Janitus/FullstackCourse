3a8882398ad4f693bb902a67f8a958ee72043387
Jani <jankuul@gmail.com>
2023-08-14 16:28:39 +0300

    puhelinluettelo

---
+++ Osa2/_puhelinluettelo/src/services/phonebook.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/persons';
+
+const getAll = () => axios.get(baseUrl);
+const create = (newPerson) => axios.post(baseUrl, newPerson);
+const remove = (id) => axios.delete(`${baseUrl}/${id}`);
+
+export default { getAll, create, remove };
