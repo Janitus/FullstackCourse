f909ef15c21760f85e694ef6797893906f5f0c89
Jani <jankuul@gmail.com>
2023-08-16 00:33:26 +0300

    Osa3 A valmis

--- Osa3/index.js
+++ Osa3/index.js
@@ -2,7 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+// HUOMIO! app.usejen järjestyksellä on väliä!
 app.use(express.json());
+
+// Morgan
+
+var morgan = require('morgan')
+//app.use(morgan('tiny'));
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+
+//app.use(unknownEndpoint)
+//app.use(requestLogger)
 
 let persons = [
   { id: 1, name: "Simo Sipuli", number: "123-4567" },
@@ -13,6 +25,10 @@ let persons = [
 ];
 
 // Gets
+
+app.get('/', function (req, res) {
+    res.send('hello, world!')
+  })
 
 app.get('/api/persons', (req, res) => {
     res.json(persons);
@@ -77,6 +93,20 @@ app.delete('/api/persons/:id', (req, res) => {
 // Search functions
 
 const nameExists = (name) => persons.some(person => person.name.toLowerCase() === name.toLowerCase());
+
+// Other functions
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
 
 // Start log
 
